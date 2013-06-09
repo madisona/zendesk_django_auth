6d24f512b2f73294468c8b5491ccf65c54a6e6e9
Aaron Madison <aaron.madison@imtapps.com>
2013-06-09 07:30:21 -0500

    added example url to use JWT login

--- zendesk_auth/urls.py
+++ zendesk_auth/urls.py
@@ -2,8 +2,9 @@
 from django.conf.urls import patterns, url
 
 
-from zendesk_auth.views import ZendeskAuthorize
+from zendesk_auth.views import ZendeskAuthorize, ZendeskJWTAuthorize
 
 urlpatterns = patterns('',
     url(r'^zendesk-authorize/$', ZendeskAuthorize.as_view(), name="zendesk-authorize"),
+    url(r'^zendesk-jwt-authorize/$', ZendeskJWTAuthorize.as_view(), name="zendesk-jwt-authorize"),
 )
