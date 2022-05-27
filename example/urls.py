bca36cb66bf0cfe79dcd59eda08041619f837274
Aaron Madison <aaron.l.madison@gmail.com>
2022-05-26 21:36:39 -0500

    Bug Fix: Fixing url import for various django versions

--- example/urls.py
+++ example/urls.py
@@ -1,4 +1,11 @@
-from django.conf.urls import include, url
+import django
+
+if django.get_version() >= '2.0.0':
+    from django.urls import re_path as url
+    from django.urls import include
+else:
+    from django.conf.urls import url
+    from django.conf.urls import include
 
 from django.contrib import admin
 from zendesk_auth import urls
